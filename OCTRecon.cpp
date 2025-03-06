ac98bc6958ce4fdde1f6624c25216faf7d6bcd1c
boonespace <boonespace@outlook.com>
2025-03-06 21:09:47 +0800

    feat(configure): add toml++ for configuration management

--- OCTRecon.cpp
+++ OCTRecon.cpp
@@ -1,13 +1,24 @@
 #include "OCTRecon.h"
+#include "toml++/toml.hpp"
 
 int main()
 {
+    // Configure
+    std::string filename = "config.toml";
+    // std::ifstream config_file(filename);
+    auto config = toml::parse_file(filename);
+    std::string path = *config["path"].value<std::string>();
+    std::string extension = *config["extension"].value<std::string>();
+    int size_x = *config["size_x"].value<int>();
+    int size_y = *config["size_y"].value<int>();
+    int size_z = *config["size_z"].value<int>();
+    
     // Adjust according to the actual project directory structure
     DataStorage ds;
-    ds.getFromFolder("../../test_data", ".bin");
+    ds.getFromFolder(path, extension);
 
     // Perform image reconstruction
-    Recon recon(2056, 280, 280, DataType::UINT16);
+    Recon recon(size_x, size_y, size_z, DataType::INT16);
 
     for (int i = 0; i < ds.length; i++)
     {
